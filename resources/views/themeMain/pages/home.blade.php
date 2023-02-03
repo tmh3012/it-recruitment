e65d567ab254ac0fbbb25c91b8d46d1177d59118
tmh3012 <78021876+tmh3012@users.noreply.github.com>
2023-02-03 15:02:07 +0700

    Update home.blade.php

--- resources/views/themeMain/pages/home.blade.php
+++ resources/views/themeMain/pages/home.blade.php
@@ -24,6 +24,8 @@
     </div>
 @endsection
 @push('js')
+    <script src="{{asset('js/ion.rangeSlider.min.js')}}"></script>
+    <script src="{{asset('js/component.range-slider.js')}}"></script>
     <script>
         $(document).ready(function () {
             //    handler ever search advanced
@@ -36,6 +38,23 @@
             btnLessBoxSearch.click(function () {
                 boxSearchAdv.slideToggle()
             })
+        });
+
+        $(document).ready(function () {
+            const ft_jobs = $('#fr_jobs');
+            const rangeSalary = $('#range_salary');
+            const min_salary = $('#min_salary');
+            const max_salary = $('#max_salary');
+
+            $('#frm-search-jobs').on('change', '#range_salary', function () {
+                let range = $(this).val().split(';');
+                min_salary.val(range[0]);
+                max_salary.val(range[1]);
+                console.log('min salary: ', min_salary.val());
+                console.log('max salary:', max_salary.val());
+            });
+
+            // function filter
         });
     </script>
 @endpush
