1f852be16e9690f45579d6e553a38f6d8a1c7d4d
tmh3012 <78021876+tmh3012@users.noreply.github.com>
2023-02-04 03:25:05 +0700

    Update topbar.blade.php
    fix error image and edit layout topbar

--- resources/views/layout/topbar.blade.php
+++ resources/views/layout/topbar.blade.php
@@ -2,7 +2,7 @@
     <ul class="list-unstyled topbar-right-menu float-right mb-0">
         <li class="dropdown notification-list d-lg-none">
             <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button"
-                aria-haspopup="false" aria-expanded="false">
+               aria-haspopup="false" aria-expanded="false">
                 <i class="dripicons-search noti-icon"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-animated dropdown-lg p-0">
@@ -11,26 +11,10 @@
                 </form>
             </div>
         </li>
-        <li class="dropdown notification-list topbar-dropdown">
-            <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button"
-                aria-haspopup="false" aria-expanded="false">
-                <img src="{{ asset('flag/us.svg') }}" alt="locale" class="mr-0 mr-sm-1" height="12">
-                <span class="align-middle d-none d-sm-inline-block">English</span> <i
-                    class="mdi mdi-chevron-down d-none d-sm-inline-block align-middle"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu" style="">
-
-                <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                    <img src="{{ asset('flag/vn.svg') }}" alt="locale" class="mr-1" height="12">
-                    <span class="align-middle">Tiếng Việt</span>
-                </a>
-            </div>
-        </li>
 
         <li class="dropdown notification-list">
             <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button"
-                aria-haspopup="false" aria-expanded="false">
+               aria-haspopup="false" aria-expanded="false">
                 <i class="dripicons-bell noti-icon"></i>
                 <span class="noti-icon-badge"></span>
             </a>
@@ -80,7 +64,7 @@
                                         <a href="javascript:void(0);" class="dropdown-item notify-item">
                                             <div class="notify-icon">
                                                 <img src=""
-                                                    class="img-fluid rounded-circle" alt="">
+                                                     class="img-fluid rounded-circle" alt="">
                                             </div>
                                             <p class="notify-details">Cristina Pride</p>
                                             <p class="text-muted mb-0 user-msg">
@@ -102,7 +86,7 @@
                                         <a href="javascript:void(0);" class="dropdown-item notify-item">
                                             <div class="notify-icon">
                                                 <img src=""
-                                                    class="img-fluid rounded-circle" alt="">
+                                                     class="img-fluid rounded-circle" alt="">
                                             </div>
                                             <p class="notify-details">Karen Robinson</p>
                                             <p class="text-muted mb-0 user-msg">
@@ -143,42 +127,35 @@
             </div>
         </li>
 
-
         <li class="dropdown notification-list">
             <a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown" href="#" role="button"
-                aria-haspopup="false" aria-expanded="false">
+               aria-haspopup="false" aria-expanded="false">
                 <span class="account-user-avatar">
-                    <img src="{{--}}{{ auth()->user()->avatar }} --}}" alt="user-image" class="rounded-circle">
+                    @if(auth()->user()->avatar)
+                        <img src="{{user()->avatar}}" alt="user-image" class="rounded-circle">
+                    @else
+                        <i class="mdi mdi-24px mdi-account"></i>
+                    @endif
                 </span>
                 <span>
-                    <span class="account-user-name">Dominic Keller</span>
-                    <span class="account-position">Founder</span>
+                    <span class="account-user-name">{{user()->name}}</span>
+                    <span class="account-position">{{\App\Enums\UserRoleEnum::getKey(user()->role)}}</span>
                 </span>
             </a>
             <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown"
-                style="">
+                 style="">
                 <!-- item-->
                 <div class=" dropdown-header noti-title">
-                    <h6 class="text-overflow m-0">Welcome !</h6>
+                    <h6 class="text-overflow m-0">Welcome {{user()->name}} !</h6>
                 </div>
 
                 <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                    <i class="mdi mdi-account-circle mr-1"></i>
-                    <span>My Account</span>
-                </a>
-
-                <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                    <i class="mdi mdi-account-edit mr-1"></i>
-                    <span>Settings</span>
-                </a>
-
-                <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
-                    <i class="mdi mdi-lifebuoy mr-1"></i>
-                    <span>Support</span>
-                </a>
+                @if(isHr())
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i class="mdi mdi-account-circle mr-1"></i>
+                        <span>My Account</span>
+                    </a>
+                @endif
 
                 <!-- item-->
                 <a href="javascript:void(0);" class="dropdown-item notify-item">
@@ -187,7 +164,7 @@
                 </a>
 
                 <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <a href="{{route('logout')}}" class="dropdown-item notify-item">
                     <i class="mdi mdi-logout mr-1"></i>
                     <span>Logout</span>
                 </a>
@@ -242,7 +219,7 @@
                 <a href="javascript:void(0);" class="dropdown-item notify-item">
                     <div class="d-flex">
                         <img class="d-flex me-2 rounded-circle" src=""
-                            alt="Generic placeholder image" height="32">
+                             alt="Generic placeholder image" height="32">
                         <div class="w-100">
                             <h5 class="m-0 font-14">Erwin Brown</h5>
                             <span class="font-12 mb-0">UI Designer</span>
@@ -254,7 +231,7 @@
                 <a href="javascript:void(0);" class="dropdown-item notify-item">
                     <div class="d-flex">
                         <img class="d-flex me-2 rounded-circle" src=""
-                            alt="Generic placeholder image" height="32">
+                             alt="Generic placeholder image" height="32">
                         <div class="w-100">
                             <h5 class="m-0 font-14">Jacob Deo</h5>
                             <span class="font-12 mb-0">Developer</span>
