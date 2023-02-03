351f8755a52efed7aaf40dc7230599333531b9e5
tmh3012 <78021876+tmh3012@users.noreply.github.com>
2023-02-04 03:31:16 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Enums\UserRoleEnum;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
@@ -18,30 +20,21 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function register()
-    {
-        $roles = UserRoleEnum::getRolesForRegister();
-
-        return view('auth.register', [
-            'roles' => $roles,
-        ]);
-    }
-
     public function handlerLogin(Request $request): RedirectResponse
     {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
-            'password' => ['required'],
+            'password' => ['required','min:8'],
         ]);
-
-
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             $getRole = (auth()->user()->role);
             $role = strtolower(UserRoleEnum::getKey($getRole));
-            return redirect()->route('admin.posts.index');
+            return redirect()->route("${role}.index");
         } else {
-            return back();
+            return redirect()->back()
+                ->withInput()
+                ->withErrors(['msg' => 'The Message']);
         }
     }
 
@@ -53,7 +46,6 @@ class AuthController extends Controller
             ->first();
         $checkExist = true;
 
-
         if (is_null($user)) {
             $user = new User();
             $user->email = $data->getEmail();
@@ -63,34 +55,79 @@ class AuthController extends Controller
 
         $user->name = $data->getName();
         $user->avatar = $data->getAvatar();
-
         auth()->login($user, true);
         if ($checkExist) {
             $role = strtolower(UserRoleEnum::getKey($user->role));
-            return redirect()->route('admin.posts.index');
+            return redirect()->route($role.'.index');
         }
         return redirect()->route('register');
     }
 
+    public function register()
+    {
+        $roles = UserRoleEnum::getRolesForRegister();
+        return view('auth.register', [
+            'roles' => $roles,
+        ]);
+    }
+
     public function registering(Request $request)
     {
-        $password = Hash::make($request->get('password'));
-        $role = $request->get('role');
         if (auth()->check()) {
+            $infoUpdate = $request->validate([
+                'role' => [
+                    'required',
+                    Rule::in(UserRoleEnum::getRolesForRegister())
+                ],
+                'password' => [
+                    'required',
+                    'min:8',
+                    'required_with:password_confirmation',
+                ],
+                'password_confirmation' => [
+                    'required',
+                    'min:8',
+                ],
+            ]);
+            $infoUpdate['password'] = Hash::make($infoUpdate['password']);
+            $role = strtolower(UserRoleEnum::getKey($infoUpdate['role']));
             User::query()
                 ->where('id', auth()->user()->id)
                 ->update([
-                    'password' => $password,
-                    'role' => $role,
+                    'password' => $infoUpdate['password'] ,
+                    'role' => $infoUpdate['role'],
                 ]);
+            redirect()->route("${role}.index");
         } else {
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => $password,
-                'role' => $request->role,
+            $data = $request->validate([
+                'name' => [
+                    'required',
+                    'string',
+                ],
+                'email' => [
+                    'required',
+                    'email:rfc,dns',
+                    Rule::unique(User::class)
+                ],
+                'role' => [
+                    'required',
+                    Rule::in(UserRoleEnum::getRolesForRegister())
+                ],
+                'password' => [
+                    'required',
+                    'min:8',
+                    'required_with:password_confirmation',
+                ],
+                'password_confirmation' => [
+                    'required',
+                    'min:8',
+                ],
             ]);
+            $data['password'] = Hash::make($data['password']);
+            $role = strtolower(UserRoleEnum::getKey($data['role']));
+            $user = User::create($data);
             Auth::login($user);
+            return redirect()->route("${role}.index");
         }
     }
 
