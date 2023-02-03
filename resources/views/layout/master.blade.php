ec37470391d808fac7b2e9f45d49380605de0e41
tmh3012 <78021876+tmh3012@users.noreply.github.com>
2023-02-03 15:01:56 +0700

    Update master.blade.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -74,7 +74,7 @@
 <script src="{{ asset('js/helper.js') }}"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/35.4.0/classic/ckeditor.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.js"></script>
-<script src="{{asset('js/validater.js')}}"></script>
+<script src="{{asset('js/validator.js')}}"></script>
 <script>
     window.editors = {};
     document.querySelectorAll('.ckeditor').forEach((el, index) => {
